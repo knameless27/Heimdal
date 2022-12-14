e0fbbc612d9a9c9fa9906956c9c4d30d4dd1144c
knameless27 <knameless27@gmail.com>
2022-12-14 17:57:49 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import navbar from './components/navbar';
+
+function App() {
+  return (
+    <div>
+      {navbar()}
+      <h1>XD</h1>
+    </div>
+  );
+}
+
+export default App;
