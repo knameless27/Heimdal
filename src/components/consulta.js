25c3a11255c44b490ea9606e6184c7a22de9ef13
knameless27 <knameless27@gmail.com>
2022-12-19 12:00:10 -0500

    avanzardos

--- src/components/consulta.js
+++ src/components/consulta.js
@@ -1,23 +1,25 @@
-import axios from '../axios';
 import { Form, Button, Container, Row, Col } from 'react-bootstrap';
+import React, {useState} from 'react';
 
 
 function Consulta() {
-  let nombre;
-  
+  let [usuario, setUsuario] = useState({})
+
 
   function buscarUsuario(e) {
     console.log(e)
     e.preventDefault();
     if (e.target[0].value !== undefined) {
-      axios.nombre(e.target[0].value).then((res) => {
-        console.log(res)
-      }).catch((error) =>{
-        console.log(error)
-      });
+      // axios.nombre(e.target[0].value).then((res) => {
+      //   setUsuario(res)
+      //   console.log(usuario)
+      //   console.log(usuario.id)
+      // }).catch((error) => {
+      //   console.log(error)
+      // });
     }
   }
-  
+
   return (
     <Container>
       <Row><Col><hr /></Col></Row>
@@ -26,7 +28,7 @@ function Consulta() {
           <Form onSubmit={buscarUsuario}>
             <Form.Group className="mb-3" controlId="formBasicEmail">
               <Form.Label>¡Ingresa tu nombre de invocador!</Form.Label>
-              <Form.Control type="text" placeholder="Nombre" value={nombre} />
+              <Form.Control type="text" placeholder="Nombre" />
             </Form.Group>
             <Button variant="primary" type="submit">
               Buscar
@@ -35,6 +37,9 @@ function Consulta() {
         </Col>
       </Row>
       <Row>
+        <p>{usuario?.name}</p>
+        <p>{usuario?.id}</p>
+        <p>{usuario?.profileIconId}</p>
       </Row>
     </Container>
   );
